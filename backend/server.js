b64c84ec53a98c56653618a6980c26da4fabe667
MarginalpersonQQ <gang.terry3@gmail.com>
2024-12-19 19:46:20 +0800

    updata 12/19 19:46

--- backend/server.js
+++ backend/server.js
@@ -84,17 +84,16 @@ app.put('/api/update/customer' , (req, res) => {
     const query = `
         UPDATE 客戶資料表 
         SET
-            客戶名稱 = ?,
-            客戶電話1 = ?,
-            客戶電話2 = ?,
-            客戶電話3 = ?,
-            聯絡人= ?,
-            統一編號 = ?,
-            地址 = ?,
-            新增日期 = ?,
-            備註 = ?
-            WHERE 客戶ID = ?
-        `;
+        客戶名稱 = '方廷原機器人',
+        客戶電話1 = '0911024537',
+        客戶電話2 = '',
+        客戶電話3 = '',
+        聯絡人= '方廷原',
+        統一編號 = '2200000',
+        地址 = '台南市安定區海寮里18之6號',
+        新增日期 = '2024-12-19T00:00:00.000Z',
+        備註 = ''
+        WHERE 客戶ID = NULL`;
 
     db.query(query, [name, phone1, phone2, phone3, contentperson, taxid, address, addingtime, remark, customerID], (err, result) => {
         try{
